6acca346751b8d0fbb69517775175884eb20d128
Ryan Kjesbo-Johnson <Ryanmark86@gmail.com>
2023-10-27 17:04:37 -0500

    some css formatting complete

--- script.js
+++ script.js
@@ -7,7 +7,29 @@ function onReady() {
 function handleSubmit(event){
     event.preventDefault();
     console.log("This worked"); 
-    let firstName = document.getElementById(firstName);
-    let lastName = document.getElementById(lastName);
-    let 
+    let firstName = document.getElementById('first-Name').value;
+    let lastName = document.getElementById('last-Name').value;
+    let identification = document.getElementById('identification-input').value;
+    let title = document.getElementById('title-input').value;
+    let annualSalary = document.getElementById('annualSalary-input').value;
+    console.log("console log of the five data entry fields", firstName, lastName, identification, title, annualSalary); //test accessing the inputs of the five input fields
+    tableBody.innerHTML += 
+    `<tr>
+    <th>${firstName}</th>
+    <th>${lastName}</th>
+    <th>${identification}</th>
+    <th>${title}</th>
+    <th>${annualSalary}</th>
+    <th><button onclick="deleteRecord(event)">␡</button></th>
+  </tr>`
+  document.getElementById('first-Name').value = '';
+  document.getElementById('last-Name').value = '';
+  document.getElementById('identification-input').value = '';
+  document.getElementById('title-input').value = '';
+  document.getElementById('annualSalary-input').value = '';
+  
+}
+
+function deleteRecord(event){
+    event.target.parentElement.parentElement.remove();
 }
