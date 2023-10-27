0bf90c033e4b0841e21333267907cd6b727b5108
Ryan Kjesbo-Johnson <Ryanmark86@gmail.com>
2023-10-27 15:54:25 -0500

    finished the form and table on html file, and created js & HTML file

---
+++ script.js
@@ -0,0 +1,13 @@
+onReady();
+
+function onReady() {
+  console.log('Javascript is working!');
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    console.log("This worked"); 
+    let firstName = document.getElementById(firstName);
+    let lastName = document.getElementById(lastName);
+    let 
+}
