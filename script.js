b5399dd0093efa8743bb62274e68767ec3a12557
Ryan Kjesbo-Johnson <Ryanmark86@gmail.com>
2023-10-27 20:26:09 -0500

    additional formatting, salary addition working

--- script.js
+++ script.js
@@ -4,15 +4,23 @@ function onReady() {
   console.log('Javascript is working!');
 }
 
+let totalSalary = 0;
+
 function handleSubmit(event){
     event.preventDefault();
     console.log("This worked"); 
+    //pull in each value from form,
+    //and set variables.
     let firstName = document.getElementById('first-Name').value;
     let lastName = document.getElementById('last-Name').value;
     let identification = document.getElementById('identification-input').value;
     let title = document.getElementById('title-input').value;
     let annualSalary = document.getElementById('annualSalary-input').value;
-    console.log("console log of the five data entry fields", firstName, lastName, identification, title, annualSalary); //test accessing the inputs of the five input fields
+   
+    //test accessing the inputs of the five input fields
+    console.log("console log of the five data entry fields", firstName, lastName, identification, title, annualSalary); 
+   
+    //input of table item for employee record
     tableBody.innerHTML += 
     `<tr>
     <th>${firstName}</th>
@@ -22,6 +30,13 @@ function handleSubmit(event){
     <th>${annualSalary}</th>
     <th><button onclick="deleteRecord(event)">␡</button></th>
   </tr>`
+
+let totalAnnualSalary = document.getElementById('total-annual-salary').textContent;
+totalSalary = Number(totalAnnualSalary) + Number(annualSalary);
+console.log(totalSalary);
+document.getElementById('total-annual-salary').textContent = totalSalary;
+
+
   document.getElementById('first-Name').value = '';
   document.getElementById('last-Name').value = '';
   document.getElementById('identification-input').value = '';
@@ -33,3 +48,4 @@ function handleSubmit(event){
 function deleteRecord(event){
     event.target.parentElement.parentElement.remove();
 }
+
